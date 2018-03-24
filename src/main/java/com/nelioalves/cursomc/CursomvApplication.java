946817fbccf7bf9da5560404b6eb62a0c69464ae
jorge.lucas <jorge.lucas.14@gmail.com>
2018-03-24 12:06:58 -0300

    inserindo carga inicial da tabela

--- src/main/java/com/nelioalves/cursomc/CursomvApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomvApplication.java
@@ -1,12 +1,33 @@
 package com.nelioalves.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomvApplication {
+import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomvApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomvApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		inserindoCategorias();
+	}
+	
+	public void inserindoCategorias() {
+		Categoria informatica = new Categoria(null, "Informática");
+		Categoria escritorio = new Categoria(null, "Escritório");
+		repo.saveAll(Arrays.asList(informatica, escritorio));
 	}
 }
