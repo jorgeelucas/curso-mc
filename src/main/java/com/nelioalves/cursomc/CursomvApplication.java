c7541be9c60139b2d89de3c3cc69dabcfe3c78a8
jorge.lucas <jorge.lucas.14@gmail.com>
2018-03-24 10:07:27 -0300

    Projeto criado

---
+++ src/main/java/com/nelioalves/cursomc/CursomvApplication.java
@@ -0,0 +1,12 @@
+package com.nelioalves.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomvApplication.class, args);
+	}
+}
