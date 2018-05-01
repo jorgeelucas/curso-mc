1179373287844d65a5441ee45504c97664b57dba
jorge.lucas <jorge.lucas.14@gmail.com>
2018-05-01 13:05:08 -0300

    criando profile de desenvolvimento

---
+++ src/main/java/com/nelioalves/cursomc/config/DevConfig.java
@@ -0,0 +1,32 @@
+package com.nelioalves.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.nelioalves.cursomc.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbservice;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateDataBase() throws ParseException {
+		if ("create".equals(strategy)) {
+			return false;
+		}
+		dbservice.instantiateDataBase();
+		return true;
+	}
+	
+}
