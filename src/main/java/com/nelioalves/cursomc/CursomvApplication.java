0f2ab55394e875750ad3a2a7cee50a2fc74b61de
jorge.lucas <jorge.lucas.14@gmail.com>
2018-05-05 16:34:38 -0300

    removendo imports indesejaveis

--- src/main/java/com/nelioalves/cursomc/CursomvApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomvApplication.java
@@ -2,9 +2,7 @@ package com.nelioalves.cursomc;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class CursomvApplication implements CommandLineRunner {
