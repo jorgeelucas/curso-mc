9a0480c61f8e76f28165927c1018e14ed6a08f03
jorge.lucas <jorge.lucas.14@gmail.com>
2018-05-05 16:33:49 -0300

    ajustando heroku

--- src/main/java/com/nelioalves/cursomc/CursomvApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomvApplication.java
@@ -2,7 +2,9 @@ package com.nelioalves.cursomc;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class CursomvApplication implements CommandLineRunner {
